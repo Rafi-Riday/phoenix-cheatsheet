c1ec8df23b87d43fc6882dfc532aaca7b4d0bdbe
Rafi-Riday <rafi.riday@gmail.com>
2023-05-03 20:14:09 +0600

    Welcome to new UI (FormulaDoc UI), added collapsible component

--- src/lib/FormulaDoc/FormulaDoc.svelte
+++ src/lib/FormulaDoc/FormulaDoc.svelte
@@ -4,7 +4,7 @@
     const { title, dataSet } = mainData;
 </script>
 
-<main class="text-gray-900 p-0 flex flex-col gap-6">
+<main class="text-gray-900 p-0 flex flex-col gap-4">
     <center class="font-semibold text-3xl mt-1">{title}</center>
     <RecursiveComponent {dataSet} titleSize={0} />
 </main>
--- src/lib/FormulaDoc/RecursiveComponent.svelte
+++ src/lib/FormulaDoc/RecursiveComponent.svelte
@@ -2,74 +2,106 @@
     import { upperCaseWord } from "$lib/utilities";
     import Katex from "$lib/Katex.svelte";
     export let dataSet;
-    export let titleSize;
-    let titleSizeArray = ["2xl", "xl", "lg", "base"];
+    export let titleSize = 3;
+    let titleSizeArray = ["text-xl", "text-lg", "text-base"];
 </script>
 
-{#each dataSet as { title, img, serial, dataSet }, idx (idx)}
-    <section class="flex flex-col gap-2">
+{#each dataSet as { title, img, collapse, serial, dataSet }, idx (idx)}
+    <section class={collapse ? "collapse collapse-arrow" : ""}>
+        <!-- collapse inp -->
+        {#if collapse}
+            <input type="checkbox" class="peer min-h-0" />
+        {/if}
+        <!-- title -->
         {#if title}
             <h2
-                class="text-{titleSize <= titleSizeArray.length - 1
+                class="font-medium {titleSize <= titleSizeArray.length - 1
                     ? titleSizeArray[titleSize]
-                    : 'base'} font-medium"
+                    : 'base'} {collapse
+                    ? 'collapse-title p-0 min-h-0 border-b-2 peer-checked:mb-2'
+                    : ''}"
             >
-                {upperCaseWord(title)}
+                {@html upperCaseWord(title)}
             </h2>
         {/if}
-        {#each dataSet as formula, idxFormula (idxFormula)}
-            {#if typeof formula === "string" && formula.includes("\\text{")}
-                <!-- `\\text{$a$}`.replace(/\\text{/g, '').slice(0, -1).split('$').map((a,idx)=>idx%2) -->
-                <div class="flex flex-row justify-start items-start">
-                    {#if serial}
-                        <span class="font-semibold"
-                            >{idxFormula + 1}.&nbsp;</span
-                        >
-                    {/if}
-                    <div>
-                        {#each formula
-                            .replace(/\\text{/g, "")
-                            .slice(0, -1)
-                            .split("$") as part, idxPart (idxPart)}
-                            {#if idxPart % 2 === 0}
-                                {@html `${part.replace(/^ | $/g, "&nbsp;")}`}
-                            {:else}
-                                <Katex expression={part} />
-                            {/if}
-                        {/each}
-                    </div>
-                </div>
-            {:else if Array.isArray(formula)}
-                <div class="flex flex-row justify-start items-start">
-                    {#if serial}
-                        <span class="font-semibold"
-                            >{idxFormula + 1}.&nbsp;</span
-                        >
-                    {/if}
-                    <div>
-                        {#each formula as part, idxPart (idxPart)}
-                            {#if !Array.isArray(part)}
-                                {@html `${part.replace(/^ | $/g, "&nbsp;")}`}
-                            {:else}
-                                <Katex expression={part[0]} />
-                            {/if}
-                        {/each}
-                    </div>
-                </div>
-            {:else if typeof formula === "string" && !formula.includes("\\text{")}
-                {#if serial}
+        <!-- body -->
+        <div
+            class="flex flex-col gap-2 {collapse
+                ? 'collapse-content'
+                : title
+                ? 'px-4 mt-2'
+                : ''}"
+            style="padding-bottom: 0 !important;"
+        >
+            {#each dataSet as formula, idxFormula (idxFormula)}
+                {#if typeof formula === "string" && formula.includes("\\text{")}
+                    <!-- `\\text{$a$}`.replace(/\\text{/g, '').slice(0, -1).split('$').map((a,idx)=>idx%2) -->
                     <div class="flex flex-row justify-start items-start">
-                        {@html `<span class="font-semibold">${
-                            idxFormula + 1
-                        }.&nbsp;</span>
-                    <span>${formula}</span>`}
+                        {#if serial}
+                            <span class="font-semibold"
+                                >{idxFormula + 1}.&nbsp;</span
+                            >
+                        {/if}
+                        <div>
+                            {#each formula
+                                .replace(/\\text{/g, "")
+                                .slice(0, -1)
+                                .split("$") as part, idxPart (idxPart)}
+                                {#if idxPart % 2 === 0}
+                                    {@html `${part.replace(
+                                        /^ | $/g,
+                                        "&nbsp;"
+                                    )}`}
+                                {:else}
+                                    <Katex expression={part} />
+                                {/if}
+                            {/each}
+                        </div>
                     </div>
-                {:else}
-                    {@html `<div>${formula}</div>`}
+                {:else if Array.isArray(formula)}
+                    <div class="flex flex-row justify-start items-start">
+                        {#if serial}
+                            <span class="font-semibold"
+                                >{idxFormula + 1}.&nbsp;</span
+                            >
+                        {/if}
+                        <div>
+                            {#each formula as part, idxPart (idxPart)}
+                                {#if !Array.isArray(part)}
+                                    {@html `${part.replace(
+                                        /^ | $/g,
+                                        "&nbsp;"
+                                    )}`}
+                                {:else}
+                                    <Katex expression={part[0]} />
+                                {/if}
+                            {/each}
+                        </div>
+                    </div>
+                {:else if typeof formula === "string" && !formula.includes("\\text{")}
+                    {#if serial}
+                        <div class="flex flex-row justify-start items-start">
+                            {@html `<span class="font-semibold">${
+                                idxFormula + 1
+                            }.&nbsp;</span>
+                        <span>${formula}</span>`}
+                        </div>
+                    {:else}
+                        {@html `<div>${formula}</div>`}
+                    {/if}
+                {:else if typeof formula === "object" && !Array.isArray(formula)}
+                    <svelte:self
+                        dataSet={[formula]}
+                        titleSize={titleSize + 1}
+                    />
                 {/if}
-            {:else if typeof formula === "object" && !Array.isArray(formula)}
-                <svelte:self dataSet={[formula]} titleSize={titleSize + 1} />
-            {/if}
-        {/each}
+            {/each}
+        </div>
     </section>
 {/each}
+
+<style>
+    .collapse-title::after {
+        transform: translate(0, -8px) rotate(45deg);
+    }
+</style>
